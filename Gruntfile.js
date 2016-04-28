aa60d1c3f000c04ee426bc51419b75207cd55038
SwordXZ0 <winzero_8@hotmail.com>
2016-04-27 23:21:57 -0500

    Initial setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 		        files: [{
 		            expand: true,
 		            cwd: 'images/',
-		            src: ['**/*.{png,jpg,gif}'],
+		            src: ['*.{png,jpg,gif}'],
 		            dest: 'images/build/'
 		        }]
 		    }
